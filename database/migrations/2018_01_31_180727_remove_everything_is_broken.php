927ba0075b32104aa1a9dc0d44899778f8dc0f35
Elize Baird <nyx@nyxart.org>
2018-01-31 11:41:38 -0700

    added new blog post

--- database/migrations/2018_01_31_180727_remove_everything_is_broken.php
+++ database/migrations/2018_01_31_180727_remove_everything_is_broken.php
@@ -18,6 +18,7 @@ class RemoveEverythingIsBroken extends Migration
 		
 		\DB::table('blog_posts')->insert([
 			'title' => 'Another year, another goal',
+			'slug' => 'another-year-another-goal',
 			'body' => "Hi! It's 2018 now and I think that means it's officially been 3 years since my last blog post. Sorry about that- it's been a busy few years. I've spent most of it honing my web development and piano skills and developing a career path I can be proud of, but I also got married and finally found the chance to transition too so I'd say overall it's been a pretty positive stretch in my life. I'm excited to share with you all some upcoming music I have planned- including live performances of Chopin's Ballade 1 & Rachmaninoffs rendition of Kreislers \"Leibesleid\". I hope this year will be as productive as I believe it will be, but only time will tell... Keep posted for fantastically creative 2018.",
 			'published_on' => '2018-01-31',
 			'created_at' => date("Y-m-d H:i:s"),
